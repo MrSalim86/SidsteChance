16d5756f808ae4f44fcc08dd3753a181a163f2d1
MrSalim86 <cph-ms1068@cphbusiness.dk>
2022-09-06 23:16:52 +0200

    laver udskrive fra starten

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,30 @@ public class Main
 {
     public static void main(String[] args) {
 
+        Node node1 = new Node("hej");
+        Node node2 = new Node("med");
+        Node node3 = new Node("dig");
+
+        node1.next = node2;
+        node2.next = node3;
+
+        node3.previous = node2;
+        node2.previous = node1;
+
+        Node n = node1;
+
+        printFromHead(n);
+
+
+    }
+
+    private static void printFromHead(Node n) {
+
+        while (n != null) {
+
+            System.out.println(n.data);
+
+            n = n.next;
+        }
     }
 }
